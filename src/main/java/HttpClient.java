c0a994881937d031556a8ecc7c1af4213ecf0488
andrey <harley3689@gmail.com>
2022-11-10 21:05:03 +0300

    "add files"

---
+++ src/main/java/HttpClient.java
@@ -0,0 +1,33 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.util.List;
+
+public class HttpClient {
+    public static final String REQUEST_URI = "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+    public static final ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) throws IOException {
+
+        try (CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)
+                        .setSocketTimeout(30000)
+                        .setRedirectsEnabled(false)
+                        .build())
+                .build()) {
+            HttpGet request = new HttpGet(REQUEST_URI);
+            CloseableHttpResponse response = httpClient.execute(request);
+            List<Cats> facts = mapper.readValue(response.getEntity().getContent(), new TypeReference<>() {});
+            facts.stream()
+                    .filter(fact -> fact.getUpvotes() != null)
+                    .forEach(System.out::println);
+        }
+    }
+}
